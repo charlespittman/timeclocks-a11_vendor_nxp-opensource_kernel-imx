e7de82ab98550a98539f8abfd897b9b0a0686398
Roberto Sartori <roberto.sartori@kynetics.com>
2022-12-13 10:39:44 +0000

    logo: add SmartLinx logo
    Signed-off-by: Roberto Sartori <roberto.sartori@kynetics.com>

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -100,6 +100,10 @@ const struct linux_logo * __ref fb_find_logo(int depth)
 		/* SuperH Linux logo */
 		logo = &logo_superh_clut224;
 #endif
+#ifdef CONFIG_LOGO_SLX_CLUT224
+                /* Smartlinx Linux logo */
+                logo = &logo_slx_clut224;
+#endif
 #ifdef CONFIG_LOGO_CUSTOM_CLUT224
 		/* Custom Linux logo */
 		logo = &logo_custom_clut224;
